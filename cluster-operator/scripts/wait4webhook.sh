f9434b938e0facb88ae61a76a1360dbca7b28c24
Ravi Singhal <ravi.r.singhal@oracle.com>
2023-03-14 18:13:03 +0530

    add timeout for webhook polling

--- cluster-operator/scripts/wait4webhook.sh
+++ cluster-operator/scripts/wait4webhook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022, Oracle and/or its affiliates.
+# Copyright (c) 2022, 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 # While loop for verrazzano-cluster-operator to wait for webhooks to be started before starting up
-while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-cluster-operator-webhook:443/validate-clusters-verrazzano-io-v1alpha1-verrazzanomanagedcluster -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
+./poll_webhook.sh "https://verrazzano-cluster-operator-webhook:443/validate-clusters-verrazzano-io-v1alpha1-verrazzanomanagedcluster"
---
+++ tools/scripts/poll_webhook.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright (c) 2023, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+SECONDS=0
+MAX_SECONDS=30
+while [ $SECONDS -lt $MAX_SECONDS ]; do
+    http_code=$(curl --insecure -s -o /dev/null -w '%{http_code}' -H 'Content-Type: application/json $1')
+    echo "$1 returned HTTP $http_code."
+    if [[ "$http_code" != "200" ]]; then
+        echo "waiting"
+        sleep 5
+    else
+        exit 0
+    fi
+done
+exit 1
