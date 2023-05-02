41c21956addd8bacfe72001487cf4419bdcb93b3
Neeraj Singh <neeraj.n.singh@oracle.com>
2023-05-02 08:08:44 +0530

    adde remaining images

--- tests/e2e/config/scripts/create_kind_cluster.sh
+++ tests/e2e/config/scripts/create_kind_cluster.sh
@@ -20,11 +20,11 @@ CALICO_SUFFIX=""
 
 create_kind_cluster() {
   if [ "${K8S_VERSION}" == "1.21" ]; then
-    KIND_IMAGE="v1.21.12@sha256:f316b33dd88f8196379f38feb80545ef3ed44d9197dca1bfd48bcb1583210207"
+    KIND_IMAGE="ghcr.io/verrazzano/kind:v1.21.14-20230502014550-bd4d75ca"
   elif [ "${K8S_VERSION}" == "1.22" ]; then
-    KIND_IMAGE="v1.22.9@sha256:8135260b959dfe320206eb36b3aeda9cffcb262f4b44cda6b33f7bb73f453105"
+    KIND_IMAGE="ghcr.io/verrazzano/kind:v1.22.15-20230502014229-bd4d75ca"
   elif [ "${K8S_VERSION}" == "1.23" ]; then
-    KIND_IMAGE="v1.23.6@sha256:b1fa224cc6c7ff32455e0b1fd9cbfd3d3bc87ecaa8fcb06961ed1afb3db0f9ae"
+    KIND_IMAGE="ghcr.io/verrazzano/kind:v1.21.14-20230502014550-bd4d75ca"
   elif [ "${K8S_VERSION}" == "1.24" ]; then
     KIND_IMAGE="ghcr.io/verrazzano/kind:v1.24.10-20230501225451-bd4d75ca"
   else
