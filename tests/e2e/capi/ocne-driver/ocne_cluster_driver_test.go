ea76c5ba891c5db09c8bca3a57b9d637d7d9bc4b
Marco Gellecanao <marco.m.gellecanao@oracle.com>
2023-05-16 15:16:51 -0700

    WAIT_TIMEOUT parameter

--- tests/e2e/capi/ocne-driver/ocne_cluster_driver_test.go
+++ tests/e2e/capi/ocne-driver/ocne_cluster_driver_test.go
@@ -29,7 +29,6 @@ import (
 const (
 	shortWaitTimeout             = 5 * time.Minute
 	shortPollingInterval         = 10 * time.Second
-	waitTimeout                  = 30 * time.Minute
 	pollingInterval              = 30 * time.Second
 	clusterName                  = "strudel2"
 	createClusterPayloadTemplate = `{
