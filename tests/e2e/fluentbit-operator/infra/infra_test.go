0ac50fc563245c373b4017345b83801986da1ab4
Isha Girdhar <isha.girdhar@oracle.com>
2023-06-12 18:31:07 +0530

    Add test

--- tests/e2e/fluentbit-operator/infra/infra_test.go
+++ tests/e2e/fluentbit-operator/infra/infra_test.go
@@ -35,20 +35,17 @@ func (u FluentBitOperatorEnabledModifier) ModifyCR(cr *vzapi.Verrazzano) {
 }
 
 const (
-	systemNamespace             = "verrazzano-system"
 	longWaitTimeout             = 20 * time.Minute
 	longPollingInterval         = 20 * time.Second
 	shortPollingInterval        = 10 * time.Second
 	shortWaitTimeout            = 5 * time.Minute
-	pollingInterval             = 10 * time.Second
-	waitTimeout                 = 20 * time.Minute
 	fluentBitComponentLabel     = "app.kubernetes.io/name"
-	fluentBitOperatorLabelValue = "fluentbit-operator"
-	fluentBitLabelValue         = "fluentbit"
+	fluentBitOperatorLabelValue = "fluent-operator"
+	fluentBitLabelValue         = "fluent-bit"
 )
 
 var (
-	t = framework.NewTestFramework("install")
+	t = framework.NewTestFramework("infra")
 )
 var _ = t.AfterEach(func() {})
 
@@ -57,28 +54,19 @@ var _ = BeforeSuite(beforeSuite)
 var beforeSuite = t.BeforeSuiteFunc(func() {
 
 	m := FluentBitOperatorEnabledModifier{}
-	update.UpdateCRWithRetries(m, pollingInterval, waitTimeout)
+	update.UpdateCR(m)
 
 	// GIVEN a VZ custom resource in dev profile,
-	// WHEN Jaeger operator is enabled,
-	// THEN Jaeger operator and pods for jaeger-collector and jaeger-query components gets created.
+	// WHEN FluentBit operator is enabled,
+	// THEN Fluent operator and pods for fluentbit-operator components gets created.
 	update.ValidatePods(fluentBitOperatorLabelValue, fluentBitComponentLabel, constants.VerrazzanoSystemNamespace, 1, false)
 	update.ValidatePods(fluentBitLabelValue, fluentBitComponentLabel, constants.VerrazzanoSystemNamespace, 1, false)
 })
 
-//	var _ = t.Describe("Verify fluentbit and configure VZ", func() {
-//		t.It("verify fluentbit pods are ready", func() {
-//			// Check all pods with fluentbit prefix
-//			Eventually(func() bool {
-//				isReady, err := pkg.PodsRunning(systemNamespace, []string{clusterName})
-//				if err != nil {
-//					return false
-//				}
-//				return isReady
-//			}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "FluentBit failed to get to ready state")
-//		})
-//	})
-var _ = t.Describe("Verify OpenSearch infra", func() {
+// GIVEN a VZ custom resource in dev profile,
+// WHEN FluentBit operator is enabled,
+// THEN expect the OpenSearch index for the verrazzano-system exists
+var _ = t.Describe("Verify FluentBit infra", func() {
 	t.It("verrazzano-system index is present", func() {
 		Eventually(func() bool {
 			return pkg.LogIndexFound("verrazzano-system")
@@ -92,20 +80,20 @@ var _ = t.Describe("Verify OpenSearch infra", func() {
 			// THEN verify that at least one recent log record is found
 			t.It("Verify recent Opensearch log record exists", func() {
 				Eventually(func() bool {
-					return pkg.LogRecordFound("verrazzano-system", time.Now().Add(-24*time.Minute), map[string]string{
+					return pkg.LogRecordFound("verrazzano-system", time.Now().Add(-5*time.Minute), map[string]string{
 						"kubernetes.container_name": "verrazzano-authproxy"})
-				}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "Expected to find a recent log record for container hello-helidon-container")
+				}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "Expected to find a recent log record for authproxy container")
 			})
 		},
 		func() {
 			// GIVEN an application with logging enabled
-			// WHEN the log records are retrieved from the Openearch index for other-container
+			// WHEN the log records are retrieved from the Opensearch index for other-container
 			// THEN verify that at least one recent log record is found
 			t.It("Verify recent Opensearch log record of other-container exists", func() {
 				Eventually(func() bool {
-					return pkg.LogRecordFound("verrazzano-system", time.Now().Add(-24*time.Minute), map[string]string{
+					return pkg.LogRecordFound("verrazzano-system", time.Now().Add(-5*time.Minute), map[string]string{
 						"kubernetes.container_name": "verrazzano-authproxy"})
-				}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "Expected to find a recent log record for other-container")
+				}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "Expected to find a recent log record for authproxy container")
 			})
 		},
 	)
